8a18fb500b966db1955d0f741d01cddfedac1661
chrisclemenz <chris.clemenz@gmail.com>
2023-03-16 15:20:04 +0100

    connectedness & large set of levels

--- EvaluateLevels.py
+++ EvaluateLevels.py
@@ -8,12 +8,67 @@ Created on Wed Mar 15 20:25:07 2023
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+from collections import deque
+
+def swap_cols(arr, frm, to):
+    arr[:,[frm, to]] = arr[:,[to, frm]]
+
+
+def findIslands(board,indices):
+    visited = set()
+    queue = deque()
+    notVisited = set()
+    for index in indices:
+        floors = np.argwhere(board == index)
+        swap_cols(floors, 0, 1)
+        notVisited.update(map(tuple, floors))
+    islands = []
+    while len(notVisited)>0:
+        start = list(notVisited)[0]
+        queue.append(start)
+        visited.add(start)
+        notVisited.remove(start)
+        island = [start]
+        while queue:
+            element = queue.popleft()
+            y = element[1]
+            x = element[0]
+            
+            up = (x,y-1)
+            down = (x,y+1)
+            left = (x-1,y)
+            right = (x+1,y)
+                    
+            if up not in visited and all(ele >= 0 for ele in up) and all(ele < len(board) for ele in up) and board[up[1]][up[0]] in indices:
+                visited.add(up)
+                notVisited.discard(up)
+                queue.append(up)
+                island.append(up)
+            if down not in visited and all(ele >= 0 for ele in down) and all(ele < len(board) for ele in down) and board[down[1]][down[0]] in indices:
+                visited.add(down)
+                notVisited.discard(down)
+                queue.append(down)
+                island.append(down)
+            if left not in visited and all(ele >= 0 for ele in left) and all(ele < len(board) for ele in left) and board[left[1]][left[0]] in indices:
+                visited.add(left)
+                notVisited.discard(left)
+                queue.append(left)
+                island.append(left)
+            if right not in visited and all(ele >= 0 for ele in right) and all(ele < len(board) for ele in right) and board[right[1]][right[0]] in indices:
+                visited.add(right)
+                notVisited.discard(right)
+                queue.append(right)
+                island.append(right)
+        islands.append(island)
+
+    return islands
+
 np.set_printoptions(linewidth=100)
 path = 'Levels'
 
 leniencies = []
 openness = []
+connectedness = []
 
 for level in os.listdir(path):
     print(level)
@@ -71,31 +126,44 @@ for level in os.listdir(path):
     #openness
     wallID = 16
     floorID = 4
+    door1ID = 2
+    door2ID = 3
     walls = np.count_nonzero(data == 16)
     floors = np.count_nonzero(data == 4)
     openness.append(floors/walls)
     #connectedness
-    
+    rooms = findIslands(data, {floorID})
+    connected_rooms = findIslands(data, {floorID,door1ID,door2ID})
+    connectedness.append(len(connected_rooms)/len(rooms))
+    #print(len(connected_rooms)/len(rooms))
     
 leniencies = np.array(leniencies)
-print(leniencies)
+#print(leniencies)
 ratio = 2/(np.max(leniencies)-np.min(leniencies)) 
 shift = (np.max(leniencies)+np.min(leniencies))/2 
-print(np.max(leniencies))
-print(np.min(leniencies))
-print(ratio)
-print(shift)
+#print(np.max(leniencies))
+#print(np.min(leniencies))
+#print(ratio)
+#print(shift)
 leniencies = (leniencies - shift)*ratio
-print(leniencies)
+#print(leniencies)
 
 shift = np.max(openness) - np.min(openness)
 openness = (openness - np.min(openness))/shift
 #print( (openness - np.min(openness))/shift)
 plt.hist(leniencies)
+plt.title("leniency")
 plt.show()
-plt.hist(openness)
-plt.show
 
-plt.hist2d(leniencies, openness, bins=(20, 20))
+plt.hist(openness)
+plt.title("openness")
+plt.show()
+
+plt.hist(connectedness)
+plt.title("connectedness")
+plt.show()
+
+plt.hist2d(openness, connectedness, bins=(40, 40))
 plt.colorbar()
+plt.title("expressive range")
 plt.show()
