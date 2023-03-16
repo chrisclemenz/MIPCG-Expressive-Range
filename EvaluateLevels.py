55a3eb46e5d215f8d13f62ee87bb6f354e2fe1e0
chrisclemenz <chris.clemenz@gmail.com>
2023-03-16 16:17:59 +0100

    added levels using smoothing

--- EvaluateLevels.py
+++ EvaluateLevels.py
@@ -63,107 +63,126 @@ def findIslands(board,indices):
 
     return islands
 
-np.set_printoptions(linewidth=100)
-path = 'Levels'
 
-leniencies = []
-openness = []
-connectedness = []
 
-for level in os.listdir(path):
-    print(level)
+def calc_expressive_range(path):
+    leniencies = []
+    openness = []
+    connectedness = []
     
-    #read in level data
-    f = open(path + '/' + level, "r")
-    dim = f.readline().split()
-    width = int(dim[0])
-    height = int(dim[1])
-    f.readline()
-    #data = [[-1] * width]*height
-    data = []
-    
-    #human objects
-    for y in range(height):
-        line = f.readline()
-        data.append(list(map(int,line.split())))
-        #print(line)
-    
-    test = f.readline()
-    
-    #AI objects
-    for y in range(height):
-        line = f.readline().split()
-        for x in range(width):
-            if int(line[x])>-1 : data[y][x] = int(line[x])
+    for level in os.listdir(path):
+        print(level)
         
-    data = np.array(data)    
-    #print(data)
+        #read in level data
+        f = open(path + '/' + level, "r")
+        dim = f.readline().split()
+        width = int(dim[0])
+        height = int(dim[1])
+        f.readline()
+        #data = [[-1] * width]*height
+        data = []
+        
+        #human objects
+        for y in range(height):
+            line = f.readline()
+            data.append(list(map(int,line.split())))
+            #print(line)
+        
+        test = f.readline()
+        
+        #AI objects
+        for y in range(height):
+            line = f.readline().split()
+            for x in range(width):
+                if int(line[x])>-1 : data[y][x] = int(line[x])
+            
+        data = np.array(data)    
+        #print(data)
+        
+        
+        chestID = 0
+        monsterID = 6
+        trapID = 12
+        
+        #leniency
+        chests = 0
+        monsters = 0
+        traps = 0
+        
+        for row in data:
+            for cell in row:
+                if cell == chestID: chests+=1
+                if cell == monsterID: monsters+=1
+                if cell == trapID: traps+=1
+    
+        #print("chests: " + str(chests))
+        #print("monsters: " + str(monsters))
+        #print("traps:" + str(traps))
+        
+        total = chests + monsters + traps
+        leniency = chests - monsters - traps
+        #print("leniency" + str(leniency))
+        leniencies.append(leniency/total)
+        
+        #openness
+        wallID = 16
+        floorID = 4
+        door1ID = 2
+        door2ID = 3
+        walls = np.count_nonzero(data == 16)
+        floors = np.count_nonzero(data == 4)
+        openness.append(floors/(walls+floors))
+        #connectedness
+        rooms = findIslands(data, {floorID})
+        connected_rooms = findIslands(data, {floorID,door1ID,door2ID})
+        connectedness.append(len(connected_rooms)/len(rooms))
+        #print(len(connected_rooms)/len(rooms))
+        
+    leniencies = np.array(leniencies)
+    #print(leniencies)
+    ratio = 2/(np.max(leniencies)-np.min(leniencies)) 
+    shift = (np.max(leniencies)+np.min(leniencies))/2 
+    #print(np.max(leniencies))
+    #print(np.min(leniencies))
+    #print(ratio)
+    #print(shift)
+    #leniencies = (leniencies - shift)*ratio
+    #print(leniencies)
+    
+    #shift = np.max(openness) - np.min(openness)
+    #print( (openness - np.min(openness))/shift)
+    plt.hist(leniencies,range=[-1,1], bins= 50)
+    plt.xlabel('leniency', fontsize=20)
+    plt.title("")
+    plt.show()
+    
+    plt.hist(openness,range = [0,1],bins= 200)
+    plt.xlabel('openness', fontsize=20)
+    plt.title("")
+    plt.show()
+    
+    plt.hist(connectedness, range = [0,1],bins= 50)
+    plt.xlabel('connectedness', fontsize=20)
+    plt.title("")
+    plt.show()
+    
+    plt.hist2d(openness, connectedness, bins=(100, 100),range=[[0, 1], [0, 1]])
+    plt.colorbar()
+    plt.xlabel('openness', fontsize=20)
+    plt.ylabel('connectedness', fontsize=20)
+    plt.title("expressive range")
+    plt.show()
+    
+    plt.hist2d(openness, leniencies, bins=(100, 100),range=[[0, 1], [-1, 1]])
+    plt.colorbar()
+    plt.xlabel('openness', fontsize=20)
+    plt.ylabel('leniency', fontsize=20)
+    plt.title("expressive range")
+    plt.show()
     
     
-    chestID = 0
-    monsterID = 6
-    trapID = 12
     
-    #leniency
-    chests = 0
-    monsters = 0
-    traps = 0
-    
-    for row in data:
-        for cell in row:
-            if cell == chestID: chests+=1
-            if cell == monsterID: monsters+=1
-            if cell == trapID: traps+=1
-
-    #print("chests: " + str(chests))
-    #print("monsters: " + str(monsters))
-    #print("traps:" + str(traps))
-    
-    leniency = chests - monsters - traps
-    #print("leniency" + str(leniency))
-    leniencies.append(leniency)
-    
-    #openness
-    wallID = 16
-    floorID = 4
-    door1ID = 2
-    door2ID = 3
-    walls = np.count_nonzero(data == 16)
-    floors = np.count_nonzero(data == 4)
-    openness.append(floors/walls)
-    #connectedness
-    rooms = findIslands(data, {floorID})
-    connected_rooms = findIslands(data, {floorID,door1ID,door2ID})
-    connectedness.append(len(connected_rooms)/len(rooms))
-    #print(len(connected_rooms)/len(rooms))
-    
-leniencies = np.array(leniencies)
-#print(leniencies)
-ratio = 2/(np.max(leniencies)-np.min(leniencies)) 
-shift = (np.max(leniencies)+np.min(leniencies))/2 
-#print(np.max(leniencies))
-#print(np.min(leniencies))
-#print(ratio)
-#print(shift)
-leniencies = (leniencies - shift)*ratio
-#print(leniencies)
-
-shift = np.max(openness) - np.min(openness)
-openness = (openness - np.min(openness))/shift
-#print( (openness - np.min(openness))/shift)
-plt.hist(leniencies)
-plt.title("leniency")
-plt.show()
-
-plt.hist(openness)
-plt.title("openness")
-plt.show()
-
-plt.hist(connectedness)
-plt.title("connectedness")
-plt.show()
-
-plt.hist2d(openness, connectedness, bins=(40, 40))
-plt.colorbar()
-plt.title("expressive range")
-plt.show()
+path = 'Levels'
+path_smoothed = 'Levels_smoothing'
+calc_expressive_range(path)
+calc_expressive_range(path_smoothed)
